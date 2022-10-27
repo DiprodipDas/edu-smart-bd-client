a12874b562e7b73bb6100be5717061213288df7d
DiprodipDas <diprodip755@gmail.com>
2022-10-27 23:39:09 +0600

    add fav icon and header logo

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -23,6 +23,7 @@ const Header = () => {
     return (
         <Navbar className='mb-4' collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
+            <img className='rounded-5 me-2' style={{height:"30px"}} src="https://media.istockphoto.com/vectors/open-book-color-vector-template-icon-vector-id1197901679?k=20&m=1197901679&s=612x612&w=0&h=QjmxcWwrj_D_IHgwt-9dvAdChyI1Vtr8bmQquYVkLE4=" alt="" />
           <Navbar.Brand href="#home">eduSmart BD</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
