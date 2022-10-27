535075c5f60b6cb198c9f37996a2edda2f0bb3a7
DiprodipDas <diprodip755@gmail.com>
2022-10-27 14:50:02 +0600

    FAQ and blog part completed successfully.

--- src/Pages/Blog/Blog/Blog.js
+++ src/Pages/Blog/Blog/Blog.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>This is blog</h2>
-        </div>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What is Cors?</Accordion.Header>
+        <Accordion.Body>
+        Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a 
+        server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. 
+        CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting 
+        the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, 
+        the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Why we using firebase?What other options do you have to implement authentication?</Accordion.Header>
+        <Accordion.Body>
+        Firebase Hosting works out-of-the-box with Firebase services, including Cloud Functions, 
+        Authentication, Realtime Database, Cloud Firestore, and Cloud Messaging.
+         You can build powerful microservices and web apps using these complementary Firebase services. <br />
+
+         Top Alternatives to Firebase Authentication are: <br />
+         1.MongoDB. <br />
+         2.Passport. <br />
+         3.Octa. <br />
+         4.Amazon Cognito. <br />
+         5.JSON Web Token.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>How does the private route work?</Accordion.Header>
+        <Accordion.Body>
+        The private route component is similar to the public route, the only change is that redirect URL
+         and authenticate condition. If the user is not authenticated he will be redirected to the login
+         page and the user can only access the authenticated routes If he is authenticated (Logged in).
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>What is Node?How does Node Works?</Accordion.Header>
+        <Accordion.Body>
+        Node (or more formally Node. js) is an open-source, cross-platform runtime environment that allows 
+        developers to create all kinds of server-side tools and applications in JavaScript. The runtime is 
+        intended for use outside of a browser context (i.e. running directly on a computer or server OS). <br />
+
+        It is a used as backend service where javascript works on the server-side of the application. 
+        This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts 
+        javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
     );
 };
 
--- src/Pages/Faq/Faq/Faq.js
+++ src/Pages/Faq/Faq/Faq.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Faq = () => {
     return (
-        <div>
-            <h1>This is FAQ</h1>
-        </div>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What is Programming Language?</Accordion.Header>
+        <Accordion.Body>
+        A programming language is a computer language that is used by programmers (developers) to communicate 
+        with computers. It is a set of instructions written 
+        in any specific language ( C, C++, Java, Python) to perform a specific task.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>What is c++ use for?</Accordion.Header>
+        <Accordion.Body>
+        What is C++ Used For? C++ (or “C-plus-plus”) is a general-purpose programming and coding language. 
+        C++ is used in developing browsers, operating systems, and applications,
+         as well as in-game programming, software engineering, data structures, etc.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>What do you mean by computer networking?</Accordion.Header>
+        <Accordion.Body>
+        Computer networking refers to interconnected computing devices that can exchange data and share 
+        resources with each other. These networked devices use a system of rules, 
+        called communications protocols, to transmit information over physical or wireless technologies.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Why is learning robotics important?</Accordion.Header>
+        <Accordion.Body>
+        Robotics concocts students for the competitive workforce of tomorrow. 
+        Robotics allows children to work on their life skills along with social skills. These skills help them to stand out of the crowd in front of their future employers. 
+        Robotics is breaking new ground in learning methodologies around the world.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="4">
+        <Accordion.Header>Does web development is a good career?</Accordion.Header>
+        <Accordion.Body>
+        Yes, web development is a good career. Mondo's annual Tech and Digital Marketing Salary guide found 
+        “Web Developer” was the most in-demand job title in tech and one of its top-paying jobs. And, 
+        according to the U.S. Bureau of Labor, 
+        the job market for Web Developers is expected to grow 15 percent by 2026
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="5">
+        <Accordion.Header>What do app developers do?</Accordion.Header>
+        <Accordion.Body>
+        A mobile app developer uses programming languages and development skills to create, test, and 
+        develop applications on mobile devices. They work in popular operating system environments like 
+        iOS and Android and often take into account UI and UX principles when creating applications.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
     );
 };
 
