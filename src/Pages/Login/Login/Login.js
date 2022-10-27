5841edeea5cd1086991c9d07d5a7d37bf99490a8
DiprodipDas <diprodip755@gmail.com>
2022-10-27 11:39:14 +0600

    Fix Reload and redirect after login

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -5,7 +5,7 @@ import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 
@@ -16,6 +16,9 @@ const Login = () => {
     const [error,setError]=useState('');
     const {signIn}=useContext(AuthContext);
     const navigate=useNavigate();
+    const location=useLocation();
+
+    const from= location.state?.from?.pathname || '/';
 
     const handleSubmit=event=>{
         event.preventDefault();
@@ -28,7 +31,7 @@ const Login = () => {
             console.log(user)
             form.reset();
             setError('');
-            navigate('/')
+            navigate(from,{replace: true});
         })
         .catch(error=>{
             console.error(error)
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -9,25 +9,31 @@ export const AuthContext=createContext();
 const auth=getAuth(app);
 const AuthProvider = ({children}) => {
  const[user,setUser]=useState(null);
+ const[loading,setLoading]=useState(true);
 
     const providerLogin=(Provider)=>{
+        setLoading(true);
         return signInWithPopup(auth,Provider);
     }
 
     const createUser=(email,password)=>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password)
     }
     const signIn=(email,password)=>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth,email,password)
     }
 
     const logOut=()=>{
+        setLoading(true);
         return signOut(auth);
     }
 
    useEffect(()=>{
     const unsubscribe =onAuthStateChanged(auth,(currentUser)=>{
-     setUser(currentUser)
+     setUser(currentUser);
+     setLoading(false);
     });
     return ()=>{
         unsubscribe();
@@ -36,7 +42,7 @@ const AuthProvider = ({children}) => {
    },[])
 
 
-    const authInfo={user,providerLogin, logOut, createUser, signIn }
+    const authInfo={user, loading, providerLogin, logOut, createUser, signIn }
     return (
        <AuthContext.Provider value={authInfo}>
          {children}
